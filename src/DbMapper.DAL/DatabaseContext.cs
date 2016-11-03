63a2ec70dc9299c1850c125d30f2b1f23d6c5bbd
wylliambastiani <wylliam.bastiani@gmail.com>
2016-11-03 20:29:57 -0300

    Added DatabaseContext implementation

---
+++ src/DbMapper.DAL/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using DbMapper.DAL.Interfaces;
+
+namespace DbMapper.DAL
+{
+    /// <summary>
+    /// This class wrap database connection information 
+    /// </summary>
+    public class DatabaseContext : IDatabaseContext
+    {
+        /// <summary>
+        /// Connection string used to connect to database server
+        /// </summary>
+        public string ConnectionString { get; private set; }
+
+        public DatabaseContext(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+    }
+}
