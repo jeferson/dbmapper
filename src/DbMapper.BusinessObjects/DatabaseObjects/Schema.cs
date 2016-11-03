e5cc90e47e5e6f13695b876454a6633fab96c2cb
wylliambastiani <wylliam.bastiani@gmail.com>
2016-11-03 19:32:23 -0300

    Schema model added to BusinessObjects Project

---
+++ src/DbMapper.BusinessObjects/DatabaseObjects/Schema.cs
@@ -0,0 +1,8 @@
+﻿namespace DbMapper.BusinessObjects.DatabaseObjects
+{
+    public class Schema
+    {
+        public int SchemaId { get; set; }
+        public string SchemaName { get; set; }
+    }
+}
