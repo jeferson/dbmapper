2a1bccdc338b0d8869f04d376dc719d9b8f9162d
wylliambastiani <wylliam.bastiani@gmail.com>
2016-11-03 19:51:40 -0300

    Adding SqlServerDatabaseMappingDAO Integration test: GetDatabaseSchemas_OnlyDefaultSchema_ReturnsDefaultSchema

---
+++ test/DbMapper.DAL.SqlServer.Tests/SqlServerDatabaseMappingDAOTests.cs
@@ -0,0 +1,27 @@
+﻿using DbMapper.BusinessObjects.DatabaseObjects;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbMapper.DAL.SqlServer.Tests
+{
+    [TestFixture]
+    public class SqlServerDatabaseMappingDAOTests
+    {
+        [Test]
+        public void GetDatabaseSchemas_OnlyDefaultSchema_ReturnsDefaultSchema()
+        {
+            IDatabaseContext dbContext = new DatabaseContext(@"Data Source=.\SQLExpress;Initial Catalog=DbMapperTestDatabase;Integrated Security=True;ApplicationIntent=ReadOnly;Application Name=DbMapper.DAL.SqlServer.Tests;");
+
+            IDatabaseMappingDAO dbMappingDAO = new SqlServerDatabaseMappingDAO();
+
+            IEnumerable<Schema> schemas = dbMappingDAO.GetDatabaseSchemas();
+
+            Assert.AreEqual(schemas.Count(), 1);
+            Assert.AreEqual(schemas.Single().SchemaName, "dbo");
+        }
+    }
+}
